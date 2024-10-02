c2f6cbdb42cfc54954179ca8f03343a40b4ad826
Jakub Radzik <jradzik4@gmail.com>
2024-10-02 17:19:18 +0200

    Initial Commit

---
+++ ZMPAI/ZMPAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZMPAIApp.swift
+//  ZMPAI
+//
+//  Created by Jakub Radzik on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZMPAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
